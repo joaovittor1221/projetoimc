6cf57612cad6d1bd0bfb05b4da70d252095fff4b
PedroStack <pedroazevedoo54@hotmail.com>
2022-01-14 14:58:48 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,83 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput , Image,Button} from 'react-native';
+import logo from './imagens/logo.png'
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text2}>
+        <b>Calculadora de IMC</b>
+      </Text>
+      <Text style={styles.text1}>
+      <b> informe seu peso:</b>
+      </Text>
+     <TextInput style={styles.caixa}/>
+     <Text style={styles.text1}>
+     <b>informe sua  altura:</b>
+      </Text>
+     <TextInput style={styles.caixa}/>
+     
+    
       <StatusBar style="auto" />
+      
+     <button  >
+          <text>calcular</text>
+        </button>
+      <Text style={styles.text2}>
+        Resultado:
+      </Text>
+      <Image source={logo} style={{ width: 420, height: 155 }} /> 
+
+     
     </View>
+    
+  
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+   padding: '50',
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'indigo',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  caixa: {
+    padding: '50',
+    backgroundColor: '#f0f	',
+    borderColor: 'white',
+    borderWidth: '1px'
+
+  },
+ 
+  text1: {
+    padding: 10,
+   color: 'white'
+   
+  
+
+  },
+  text2: {
+    paddingBottom: 15,
+    fontSize: 25,
+   color: 'white'
+   
+  
+
+  },
+
+  bt1: {
+   padding: 10
+     
+    
+   
+   
+  
+
+  },
+  
 });
+
---
+++ componentes/caixadetexto.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default function (){
+    return(
+        <View>
+        <text> Digite seu peso</text>
+        <TextInput/>
+        </View>
+    )
+}
